c870684c90c1e6ee56dd1738cef66fe1588eaad5
Cleo550 <cleo550@gmail.com>
2026-03-12 19:27:02 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -1,3 +1,10 @@
 import streamlit as st
+from PIL import Image
 
-st.switch_page("pages/0_Facturas_Clientes.py")
+st.set_page_config(page_title="Cleo Pro", layout="centered")
+
+try:
+    logo = Image.open("logo.jpeg")
+    st.image(logo, use_container_width=True)
+except:
+    st.title("Cleo Pro")
